34c940580aacab1fc3f3e00f5fcfcf6dc63da331
salsabileslam <salsabileslam16@gmail.com>
2023-08-22 04:41:18 +0300

    splash screen ui done

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SvgPicture.asset(
+          AssetsData.logo,
+          width: 300,
+          height: 64,
+        ),
+      ],
+    );
+  }
+}
